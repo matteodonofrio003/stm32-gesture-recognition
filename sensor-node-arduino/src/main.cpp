1fcbcef837a17615ada1ece44231c52920858392
matteodonofrio003 <matteodonofrio003@gmail.com>
2026-08-11 18:27:32 +0200

    Add initial configuration files and implement MPU6050 sensor setup

--- sensor-node-arduino/src/main.cpp
+++ sensor-node-arduino/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_MPU6050 mpu;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print(a.acceleration.x);
+  Serial.print(",");
+  Serial.print(a.acceleration.y);
+  Serial.print(",");
+  Serial.print(a.acceleration.z);
+  Serial.print(",");
+  Serial.print(g.gyro.x);
+  Serial.print(",");
+  Serial.print(g.gyro.y);
+  Serial.print(",");
+  Serial.print(g.gyro.z);
+  Serial.println();
+
+  delay(10);
 }
